929b6e59ad6a000533e73ced6b9dd02e91cbaf8b
Renato Florentino Garcia <fgarcia.renato@gmail.com>
2012-08-02 22:21:22 -0300

    Supporting ROI and signed and large types.

--- cv_imshow.py
+++ cv_imshow.py
@@ -5,7 +5,6 @@ import struct
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
 
-
 class cv_imshow (gdb.Command):
     """Diplays the content of an opencv image"""
 
@@ -24,172 +23,174 @@ class cv_imshow (gdb.Command):
             self.cvmat(val)
 
     def cvmat(self, val):
-        # Parse contents.
-        flags = val["flags"]
+        flags = val['flags']
         cv_type = flags & 7
         channels = 1 + (flags >> 3) & 63;
         if cv_type == 0:
             cv_type_name = "CV_8U"
-            cv_byte_depth = 1
             cv_data_symbol = "B"
-            isfloat = False
         elif cv_type == 1:
             cv_type_name = "CV_8S"
-            cv_byte_depth = 1
             cv_data_symbol = "b"
-            isfloat = False
         elif cv_type == 2:
             cv_type_name = "CV_16U"
-            cv_byte_depth = 2
             cv_data_symbol = "H"
-            isfloat = False
         elif cv_type == 3:
             cv_type_name = "CV_16S"
-            cv_byte_depth = 2
             cv_data_symbol = "h"
-            isfloat = False
         elif cv_type == 4:
             cv_type_name = "CV_32S"
-            cv_byte_depth = 4
             cv_data_symbol = "i"
-            isfloat = False
         elif cv_type == 5:
             cv_type_name = "CV_32F"
-            cv_byte_depth = 4
             cv_data_symbol = "f"
-            isfloat = True
         elif cv_type == 6:
             cv_type_name = "CV_64F"
-            cv_byte_depth = 8
             cv_data_symbol = "d"
-            isfloat = True
         else:
             print "Sorry but the type CV_USRTYPE1 is not supported"
             return
-        rows = val["rows"]
-        cols = val["cols"]
+        rows = val['rows']
+        cols = val['cols']
+
+        line_step = val['step']['p'][0]
 
         # Print information about image.
         print "Image details: " + cv_type_name + " with # channels " + \
             str(channels) + ", rows: " + str(rows) + " cols: " + str(cols)
 
-        if channels == 1:
-            mode = "L"
-        elif channels == 3:
-            mode = "RGB"
-        else:
-            print "Number of channels not supported at the moment."
-            return
+        data_address = unicode(val['data']).encode('utf-8').split()[0]
+        data_address = int(data_address, 16)
 
-        # Read memory from the inferior.
-        infe = gdb.inferiors()
-        data_address = unicode(val["data"]).encode('utf-8').split()[0]
-        memory_data = infe[0].read_memory( \
-            int(data_address, 16), rows * cols * cv_byte_depth * channels)
-
-        # Format memory data to load into the image.
-        if channels == 1:
-            image_data = struct.unpack( \
-                "%d%s" % (rows * cols, cv_data_symbol), memory_data)
-        elif channels == 3:
-            image_data = struct.unpack( \
-                "%d%s" % (rows * cols * 3, cv_data_symbol), memory_data)
-            if isfloat:
-                # A float image is discretized in 256 bins for display.
-                max_image_data = max(image_data)
-                min_image_data = min(image_data)
-                img_range = max_image_data - min_image_data
-                if img_range > 0:
-                    image_data = [int(255 * (i - min_image_data) / img_range) \
-                                      for i in image_data]
-                else:
-                    image_data = [0 for i in image_data]
-            # OpenCV stores the channels in BGR mode. Convert to RGB while
-            # packing.
-            image_data = zip(*[image_data[i::3] for i in [2, 1, 0]])
-
-        # Show image.
-        img = Image.new(mode, (cols, rows))
-        img.putdata(image_data)
-        img.show()
-
+        self.build(cols, rows, channels, line_step, data_address, cv_data_symbol)
 
 
     def iplimage(self, val):
-
         depth = val['depth']
         channels = val['nChannels']
         if depth == 0x8:
             cv_type_name = "IPL_DEPTH_8U"
             cv_data_symbol = "B"
-            isfloat = False
+            elem_size = 1
         elif depth == -0x7FFFFFF8:
             cv_type_name = "IPL_DEPTH_8S"
             cv_data_symbol = "b"
-            isfloat = False
+            elem_size = 1
         elif depth == 0x10:
             cv_type_name = "IPL_DEPTH_16U"
             cv_data_symbol = "H"
-            isfloat = False
+            elem_size = 2
         elif depth == -0x7FFFFFF0:
             cv_type_name = "IPL_DEPTH_16S"
             cv_data_symbol = "h"
-            isfloat = False
+            elem_size = 2
         elif depth == -0x7FFFFFE0:
             cv_type_name = "IPL_DEPTH_32S"
             cv_data_symbol = "i"
-            isfloat = False
+            elem_size = 4
         elif depth == 0x20:
             cv_type_name = "IPL_DEPTH_32F"
             cv_data_symbol = "f"
-            isfloat = True
+            elem_size = 4
         elif depth == 0x40:
             cv_type_name = "IPL_DEPTH_64F"
             cv_data_symbol = "d"
-            isfloat = True
+            elem_size = 8
         else:
             print "Sorry but the type CV_USRTYPE1 is not supported"
             return
 
-        rows = int(val["height"])
-        cols = int(val["width"])
-        line_step = int(val['widthStep'])
-        padding = line_step - cols * channels
+        rows = val['height'] if str(val['roi']) == '0x0' else val['roi']['height']
+        cols = val['width'] if str(val['roi']) == '0x0' else val['roi']['width']
+        line_step = val['widthStep']
 
         # Print information about image.
-        print "Image details: " + cv_type_name + " with # channels " + \
-            str(channels) + ", rows: " + str(rows) + " cols: " + str(cols)
+        print 'Image details: ' + cv_type_name + ' with ' + str(channels) +\
+          ' channels, rows: ' + str(rows) + " cols: " + str(cols)
 
-        if channels == 1:
-            mode = "L"
-        elif channels == 3:
-            mode = "RGB"
-        else:
-            print "Number of channels not supported at the moment."
-            return
+        data_address = unicode(val['imageData']).encode('utf-8').split()[0]
+        data_address = int(data_address, 16)
+        if str(val['roi']) != '0x0':
+            x_offset = int(val['roi']['xOffset'])
+            y_offset = int(val['roi']['yOffset'])
+            data_address += line_step * y_offset + x_offset * elem_size * channels
 
-        # Read memory from the inferior.
+        self.build(cols, rows, channels, line_step, data_address, cv_data_symbol)
+
+
+    def build(self, width, height, n_channel, line_step, data_address, data_type):
+        """ Copies the image data to a PIL image and shows it.
+
+        Args:
+            width: The image width, in pixels.
+            height: The image height, in pixels.
+            n_channel: The number of channels in image.
+            line_step: The offset to change to pixel (i+1, j) being
+                in pixel (i, j), in bytes.
+            data_address: The address of image data in memory.
+            data_type: Python struct module code to the image data type.
+        """
+        width = int(width)
+        height = int(height)
+        n_channel = int(n_channel)
+        line_step = int(line_step)
+        data_address = int(data_address)
+
         infe = gdb.inferiors()
-        data_address = unicode(val["imageData"]).encode('utf-8').split()[0]
-        memory_data = infe[0].read_memory( \
-            int(data_address, 16), val['imageSize'])
+        memory_data = infe[0].read_memory(data_address, line_step * height)
 
-        image_data = []
+        # Calculate the memory padding to change to the next image line.
+        # Either due to memory alignment or a ROI.
+        if data_type in ('b', 'B'):
+            elem_size = 1
+        elif data_type in ('h', 'H'):
+            elem_size = 2
+        elif data_type in ('i', 'f'):
+            elem_size = 4
+        elif data_type == 'd':
+            elem_size = 8
+        padding = line_step - width * n_channel * elem_size
+
         # Format memory data to load into the image.
-        if channels == 1:
-            image_data = struct.unpack( \
-                "%d%s" % (rows * cols, cv_data_symbol), memory_data)
-        elif channels == 3:
-            fmt = "%d%s%dx" % (cols * 3, cv_data_symbol, padding)
+        image_data = []
+        if n_channel == 1:
+            mode = "L"
+            fmt = "%d%s%dx" % (width, data_type, padding)
+            for line in chunker(memory_data, line_step):
+                image_data.extend(struct.unpack(fmt, line))
+        elif n_channel == 3:
+            mode = "RGB"
+            fmt = "%d%s%dx" % (width * 3, data_type, padding)
             for line in chunker(memory_data, line_step):
                 image_data.extend(struct.unpack(fmt, line))
 
-            # OpenCV stores the channels in BGR mode. Convert to RGB while
-            # packing.
+        # Fit the opencv elemente data in the PIL element data
+        if data_type == 'b':
+            image_data = [i+128 for i in image_data]
+        elif data_type == 'H':
+            image_data = [i>>8 for i in image_data]
+        elif data_type == 'h':
+            image_data = [(i+32768)>>8 for i in image_data]
+        elif data_type == 'i':
+            image_data = [(i+2147483648)>>24 for i in image_data]
+        elif data_type in ('f','d'):
+            # A float image is discretized in 256 bins for display.
+            max_image_data = max(image_data)
+            min_image_data = min(image_data)
+            img_range = max_image_data - min_image_data
+            if img_range > 0:
+                image_data = [int(255 * (i - min_image_data) / img_range) \
+                              for i in image_data]
+            else:
+                image_data = [0 for i in image_data]
+
+
+        if n_channel == 3:
+            # OpenCV stores the channels in BGR mode. Convert to RGB while packing.
             image_data = zip(*[image_data[i::3] for i in [2, 1, 0]])
 
         # Show image.
-        img = Image.new(mode, (cols, rows))
+        img = Image.new(mode, (width, height))
         img.putdata(image_data)
         img.show()
 
--- test.cpp
+++ test.cpp
@@ -1,35 +1,108 @@
-#include <iostream>
-#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-#include "opencv/cv.h"
-#include "opencv/highgui.h"
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char *argv[])
 {
-    // Create images of several image types.
-    Mat img(50, 50, CV_8UC1, Scalar(0));
-    Mat img_f(5, 5, CV_32FC1, Scalar(0));
-    Mat img_2f(5, 5, CV_32FC2, Scalar(0));
-    Mat img_3f(5, 5, CV_32FC3, Scalar(0));
-    Mat img_4f(5, 5, CV_32FC4, Scalar(0));
-    Mat img_u(5, 5, CV_USRTYPE1, Scalar(0));
-    Mat img_3(500, 500, CV_8UC3, Scalar(0, 0, 0));
-    Mat img_2(5, 5, CV_8UC2, Scalar(0, 0, 0));
+    Mat mat8UC1 (250, 250, CV_8UC1,  Scalar(0));
+    Mat mat8SC1 (250, 250, CV_8SC1,  Scalar(0));
+    Mat mat16UC1(250, 250, CV_16UC1, Scalar(0));
+    Mat mat16SC1(250, 250, CV_16SC1, Scalar(0));
+    Mat mat32SC1(250, 250, CV_32SC1, Scalar(0));
+    Mat mat32FC1(250, 250, CV_32FC1, Scalar(0.0));
+    Mat mat64FC1(250, 250, CV_64FC1, Scalar(0.0));
+    rectangle(mat8UC1,  Point(50,50), Point(200,150), Scalar(150));
+    rectangle(mat8SC1,  Point(50,50), Point(200,150), Scalar(-70));
+    rectangle(mat16UC1, Point(50,50), Point(200,150), Scalar(50000));
+    rectangle(mat16SC1, Point(50,50), Point(200,150), Scalar(-20000));
+    rectangle(mat32SC1, Point(50,50), Point(200,150), Scalar(-1e9));
+    rectangle(mat32FC1, Point(50,50), Point(200,150), Scalar(-1e9));
+    rectangle(mat32FC1, Point(50,50), Point(200,150), Scalar(0.7));
+    rectangle(mat32FC1, Point(40,40), Point(210,160), Scalar(-0.7));
+    rectangle(mat64FC1, Point(50,50), Point(200,150), Scalar(0.07));
+    rectangle(mat64FC1, Point(40,40), Point(210,160), Scalar(-0.07));
 
-    // Modify images.
-    rectangle(img, Point(10, 10), Point(30, 30), Scalar(200), CV_FILLED);
-    rectangle(img_f, Point(1, 1), Point(3, 3), Scalar(255), CV_FILLED);
-    rectangle(img_3, Point(100, 100), Point(300, 300), Scalar(255, 0, 0), CV_FILLED);
+    Mat mat8UC3 (250, 250, CV_8UC3,  Scalar(0, 0, 0));
+    Mat mat8SC3 (250, 250, CV_8SC3,  Scalar(0, 0, 0));
+    Mat mat16UC3(250, 250, CV_16UC3, Scalar(0, 0, 0));
+    Mat mat16SC3(250, 250, CV_16SC3, Scalar(0, 0, 0));
+    Mat mat32SC3(250, 250, CV_32SC3, Scalar(0, 0, 0));
+    Mat mat32FC3(250, 250, CV_32FC3, Scalar(0.0, 0.0, 0.0));
+    Mat mat64FC3(250, 250, CV_64FC3, Scalar(0.0, 0.0, 0.0));
+    rectangle(mat8UC3,  Point(50,50), Point(200,150), Scalar(150,0,0));
+    rectangle(mat8SC3,  Point(50,50), Point(200,150), Scalar(-70,0,0));
+    rectangle(mat16UC3, Point(50,50), Point(200,150), Scalar(50000,0,0));
+    rectangle(mat16SC3, Point(50,50), Point(200,150), Scalar(-20000,0,0));
+    rectangle(mat32SC3, Point(50,50), Point(200,150), Scalar(-1e9,0,0));
+    rectangle(mat32FC3, Point(50,50), Point(200,150), Scalar(0.7,0,0));
+    rectangle(mat32FC3, Point(40,40), Point(210,160), Scalar(-0.7,0,0));
+    rectangle(mat64FC3, Point(50,50), Point(200,150), Scalar(0.07,0,0));
+    rectangle(mat64FC3, Point(40,40), Point(210,160), Scalar(-0.07,0,0));
 
-    // Read image from file.
-    Mat read_img = imread("gogh.jpg");
-    Mat roi_img(read_img, Rect(50, 50, 100, 100));
+    Mat matImg = imread("gogh.jpg");
+    Mat matRoi(matImg, Rect(50, 50, 100, 100));
 
-    IplImage *iplImage = cvLoadImage("gogh.jpg");
+    //--------------------------------------------------------------------------
+    IplImage *ipl8UC1  = cvCreateImage(cvSize(250, 250), IPL_DEPTH_8U,  1); cvSet(ipl8UC1,  cvScalar(0));
+    IplImage *ipl8SC1  = cvCreateImage(cvSize(250, 250), IPL_DEPTH_8S,  1); cvSet(ipl8SC1,  cvScalar(0));
+    IplImage *ipl16UC1 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_16U, 1); cvSet(ipl16UC1, cvScalar(0));
+    IplImage *ipl16SC1 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_16S, 1); cvSet(ipl16SC1, cvScalar(0));
+    IplImage *ipl32SC1 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_32S, 1); cvSet(ipl32SC1, cvScalar(0));
+    IplImage *ipl32FC1 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_32F, 1); cvSet(ipl32FC1, cvScalar(0.0));
+    IplImage *ipl64FC1 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_64F, 1); cvSet(ipl64FC1, cvScalar(0.0));
+    cvRectangle(ipl8UC1,  cvPoint(50,50), cvPoint(200,150), cvScalar(150));
+    cvRectangle(ipl8SC1,  cvPoint(50,50), cvPoint(200,150), cvScalar(-70));
+    cvRectangle(ipl16UC1, cvPoint(50,50), cvPoint(200,150), cvScalar(50000));
+    cvRectangle(ipl16SC1, cvPoint(50,50), cvPoint(200,150), cvScalar(-20000));
+    cvRectangle(ipl32SC1, cvPoint(50,50), cvPoint(200,150), cvScalar(-1e9));
+    cvRectangle(ipl32FC1, cvPoint(50,50), cvPoint(200,150), cvScalar(-1e9));
+    cvRectangle(ipl32FC1, cvPoint(50,50), cvPoint(200,150), cvScalar(0.7));
+    cvRectangle(ipl32FC1, cvPoint(40,40), cvPoint(210,160), cvScalar(-0.7));
+    cvRectangle(ipl64FC1, cvPoint(50,50), cvPoint(200,150), cvScalar(0.07));
+    cvRectangle(ipl64FC1, cvPoint(40,40), cvPoint(210,160), cvScalar(-0.07));
+
+    IplImage *ipl8UC3  = cvCreateImage(cvSize(250, 250), IPL_DEPTH_8U,  3); cvSet(ipl8UC3,  cvScalar(0, 0, 0));
+    IplImage *ipl8SC3  = cvCreateImage(cvSize(250, 250), IPL_DEPTH_8S,  3); cvSet(ipl8SC3,  cvScalar(0, 0, 0));
+    IplImage *ipl16UC3 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_16U, 3); cvSet(ipl16UC3, cvScalar(0, 0, 0));
+    IplImage *ipl16SC3 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_16S, 3); cvSet(ipl16SC3, cvScalar(0, 0, 0));
+    IplImage *ipl32SC3 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_32S, 3); cvSet(ipl32SC3, cvScalar(0, 0, 0));
+    IplImage *ipl32FC3 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_32F, 3); cvSet(ipl32FC3, cvScalar(0.0, 0.0, 0.0));
+    IplImage *ipl64FC3 = cvCreateImage(cvSize(250, 250), IPL_DEPTH_64F, 3); cvSet(ipl64FC3, cvScalar(0.0, 0.0, 0.0));
+    cvRectangle(ipl8UC3,  cvPoint(50,50), cvPoint(200,150), cvScalar(150,0,0));
+    cvRectangle(ipl8SC3,  cvPoint(50,50), cvPoint(200,150), cvScalar(-70,0,0));
+    cvRectangle(ipl16UC3, cvPoint(50,50), cvPoint(200,150), cvScalar(50000,0,0));
+    cvRectangle(ipl16SC3, cvPoint(50,50), cvPoint(200,150), cvScalar(-20000,0,0));
+    cvRectangle(ipl32SC3, cvPoint(50,50), cvPoint(200,150), cvScalar(-1e9,0,0));
+    cvRectangle(ipl32FC3, cvPoint(50,50), cvPoint(200,150), cvScalar(0.7,0,0));
+    cvRectangle(ipl32FC3, cvPoint(40,40), cvPoint(210,160), cvScalar(-0.7,0,0));
+    cvRectangle(ipl64FC3, cvPoint(50,50), cvPoint(200,150), cvScalar(0.07,0,0));
+    cvRectangle(ipl64FC3, cvPoint(40,40), cvPoint(210,160), cvScalar(-0.07,0,0));
+
+    IplImage *iplImg = cvLoadImage("gogh.jpg");
+    IplImage *iplRoi = cvLoadImage("gogh.jpg");
+    cvSetImageROI(iplRoi, cvRect(50, 50, 100, 100));
+
+    cvReleaseImage(&ipl8UC1);
+    cvReleaseImage(&ipl8SC1);
+    cvReleaseImage(&ipl16UC1);
+    cvReleaseImage(&ipl16SC1);
+    cvReleaseImage(&ipl32SC1);
+    cvReleaseImage(&ipl32FC1);
+    cvReleaseImage(&ipl64FC1);
+    cvReleaseImage(&ipl8UC3);
+    cvReleaseImage(&ipl8SC3);
+    cvReleaseImage(&ipl16UC3);
+    cvReleaseImage(&ipl16SC3);
+    cvReleaseImage(&ipl32SC3);
+    cvReleaseImage(&ipl32FC3);
+    cvReleaseImage(&ipl64FC3);
+    cvReleaseImage(&iplImg);
+    cvReleaseImage(&iplRoi);
 
     return 0;
 }
-
